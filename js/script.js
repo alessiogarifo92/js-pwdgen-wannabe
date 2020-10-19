6ab11d1bf204ff4224d6890cfbdb9a781dcf05ed
alessiogarifo92 <alessiogarifo92@gmail.com>
2020-10-19 14:46:54 +0200

    inserite var e prompt per inserire password

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// chiedo all'utente nome
+var nome= prompt("Inserisci il tuo nome");
+// chiedo all'utente cognome
+var cognome= prompt("Inserisci il tuo cognome");
+// chiedo all'utente colore preferito
+var colorepref= prompt("Inserisci il tuo colore preferito");
+// scrivo sulla pagina nomecognomecolorepreferito19
+document.getElementById('password').innerHTML = nome + cognome + colorepref + "19"
